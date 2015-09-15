a0e8fdfe662652a8d2bc9235c56216bb48b5bf25
Clinton Halpin <clintonhalpin@gmail.com>
2015-09-14 21:42:25 -0400

    Update

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ export default class Main extends React.Component{
     return (
       <div className='container'>
         <h1 className="headline fw--regular m0">Designer News NYC Meetup</h1>
-        <p className="m0"><a href="https://www.google.com/maps?q=houston+hall&es_sm=91&um=1&ie=UTF-8&sa=X&ved=0CAcQ_AUoAWoVChMIjK7At-j3xwIVw9OACh09rA0E">Houston Hall</a> &middot; 7:00 PM Thursday October 8, 2015 (EST) </p>
+        <a href="http://attending.io/events/designer-news-unofficial-nyc-meetup">NVM, I'm not building a custom page just check this...</a>
       </div>
     )
   }
